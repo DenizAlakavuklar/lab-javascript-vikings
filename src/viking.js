8a987c6efe21b55ee29eff7edc05a3475fd74ac8
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-01-13 13:15:45 +0100

    Completed Iteration 2.

--- src/viking.js
+++ src/viking.js
@@ -20,9 +20,24 @@ class Viking extends Soldier {
         super(health, strength);
         this.name = name
     }
-    
-    }
+    attack() {
+        return (this.strength)
 
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.strength !== 0) {
+            return `${this.name} has received ${damage} points of damage`
+        }
+
+        else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry() {
+        return `Odin Owns You All!`
+    }
+}
 
 
 
